410ac6089d8dbae97b590da9ad4cb228604c4d38
unknown <hebbarambika.k@gmail.com>
2024-02-06 22:31:40 +0530

    access event info and display it

--- src/components/ClubPage.js
+++ src/components/ClubPage.js
@@ -1,29 +1,46 @@
-// import React from 'react';
-// import ClubInfoSlider from './ClubInfoSlider';
-// import { useClubContext } from './context/ClubContext';
-// import { ClubProvider } from './context/ClubContext';
-import React from 'react';
-import { useClubContext } from '../context/ClubContext';
+import React, { useState } from "react";
+import { useClubContext } from "../context/ClubContext";
 
 const ClubPage = () => {
-  // const { clubInfo, eventInfo } = useClubContext();
-  const {clubInfo, eventInfo} = useClubContext();
+  const { clubInfo, eventInfo } = useClubContext();
 
   return (
     <div>
       <h1>{clubInfo.name}</h1>
       <p>{clubInfo.goal}</p>
       <img src={clubInfo.imgURL} alt={clubInfo.name} />
-      <p>Admin: {clubInfo.adminName}</p>
+      {/* <p>Admin: {clubInfo.adminName}</p> */}
 
       <h2>Events</h2>
+      {/* Iterate over events */}
       {eventInfo.map((event, index) => (
-        <div key={index}>
-          <h3>{event.title}</h3>
-          <p>{event.description}</p>
-          <p>{event.date}</p>
-        </div>
+        <Event key={index} event={event} />
       ))}
+    </div>
+  );
+};
+
+const Event = ({ event }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleMoreClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div>
+      <h3>{event.eventName}</h3>
+      {expanded && (
+        <div>
+          <p>Date: {event.eventDate}</p>
+          <p>Venue: {event.eventVenue}</p>
+          <p>Time: {event.eventTime}</p>
+          <p>Description: {event.eventDescription}</p>
+          {/* You can display additional fields here */}
+        </div>
+      )}
+      <button onClick={handleMoreClick}>{expanded ? "Less" : "More"}</button>
+      <button>Enroll</button>
     </div>
   );
 };
